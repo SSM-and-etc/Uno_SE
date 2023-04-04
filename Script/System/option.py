25ae8f6c57a20b679dede747eaa72d75d5240bcc
pianop <kphjjh723@gmail.com>
2023-04-04 16:45:56 +0900

    chore: add one line

--- Script/System/option.py
+++ Script/System/option.py
@@ -4,7 +4,8 @@ import os
 
 class Option():
     def __init__(self, root, screen_size):
-        pass
+        self.option_BG = pygame.image.load(os.path.join(root, "Material/BG/title.png"))
+        
     
     def display(self, main):
         pass
