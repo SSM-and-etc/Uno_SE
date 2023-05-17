a54d350fa2c432f73163890887d33322d70f973f
Donghun Won <wonhun1225@gmail.com>
2023-05-17 20:55:43 +0900

    fix: 플레이어 혼자 플레이되는 오류 수정 (#66)

--- Script/Lobby/button.py
+++ Script/Lobby/button.py
@@ -1,6 +1,8 @@
 import pygame
+
+
 class StartButton:
-    def __init__(self, msg, x, y, w, h, color,font, screen, action):
+    def __init__(self, msg, x, y, w, h, color, font, screen, action):
         self.msg = msg
         self.x = x
         self.y = y
@@ -17,19 +19,16 @@ class StartButton:
         text_surf, text_rect = self.text_objects(self.msg, self.font)
         text_rect.center = self.rect.center
         self.screen.blit(text_surf, text_rect)
-        
 
-    def handle_event(self,event):   
-      if event.type == pygame.MOUSEBUTTONDOWN:
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
                 self.action()
-    def handle_key_event(self,event):   
-        if event.key == pygame.K_RETURN:
-              self.action()
 
-        
+    def handle_key_event(self, event):
+        if event.key == pygame.K_RETURN:
+            self.action()
 
     def text_objects(self, text, font):
         text_surface = font.render(text, True, pygame.Color('black'))
         return text_surface, text_surface.get_rect()
-
--- Script/Lobby/lobby.py
+++ Script/Lobby/lobby.py
@@ -2,97 +2,90 @@ import pygame
 
 from Lobby.input_box import InputBox
 from Lobby.button import StartButton
+
+
 class Lobby():
-    def __init__(self,main):
+    def __init__(self, main):
         self.WHITE = (255, 255, 255)
-        self.ORANGE= (255, 163, 0)
+        self.ORANGE = (255, 163, 0)
         self.lobby_img = pygame.image.load("Material/BG/Title.png")
-        self.ratio_list=[(0.8,0.05),(0.8,0.2),(0.8,0.35),(0.8,0.5),(0.8,0.65),(0.8,0.8)]
+        self.ratio_list = [(0.8, 0.05), (0.8, 0.2), (0.8, 0.35),
+                           (0.8, 0.5), (0.8, 0.65), (0.8, 0.8)]
         self.original_size = self.lobby_img.get_rect().size
-        self.player_box_info=[]
-        self.player_list=[]
-        self.screen= main.screen
-        self.screen_width=main.screen.get_width()
-        self.screen_heigth=main.screen.get_height()
+        self.player_box_info = []
+        self.player_list = []
+        self.screen = main.screen
+        self.screen_width = main.screen.get_width()
+        self.screen_heigth = main.screen.get_height()
         self.aspect_ratio = self.screen_width / 1280, self.screen_heigth / 720
         self.new_width = int(self.original_size[0] * self.aspect_ratio[0])
-        self.new_height = int(self.original_size[1] *self.aspect_ratio[1])
-        self.lobby_img = pygame.transform.scale(self.lobby_img, (self.new_width, self.new_height))
-        self.font= pygame.font.Font(None, int(self.screen_width/30))
-        self.make_screen(self.screen_width,self.screen_heigth, self.ratio_list)
-        self.mouse_pos=pygame.mouse.get_pos()
-        self.start_button=StartButton("Start", self.screen_width*0.4, self.screen_heigth*0.6, self.screen_width/6, self.screen_heigth/10,self.ORANGE,self.font, self.screen,self.play_game)
+        self.new_height = int(self.original_size[1] * self.aspect_ratio[1])
+        self.lobby_img = pygame.transform.scale(
+            self.lobby_img, (self.new_width, self.new_height))
+        self.font = pygame.font.Font(None, int(self.screen_width/30))
+        self.make_screen(self.screen_width,
+                         self.screen_heigth, self.ratio_list)
+        self.mouse_pos = pygame.mouse.get_pos()
+        self.start_button = StartButton("Start", self.screen_width*0.4, self.screen_heigth*0.6,
+                                        self.screen_width/6, self.screen_heigth/10, self.ORANGE, self.font, self.screen, self.check_user)
         self.play_game
-        self.main=main
-        self.index=-1
-        for i,info in enumerate(self.player_box_info):
-            self.player_list.append(InputBox(info[0],info[1],info[2],info[3],i,'player'+str(i),self.font))
-    def display(self,main):
+        self.check_user
+        self.main = main
+        self.index = -1
+        self.count = 0
+        for i, info in enumerate(self.player_box_info):
+            self.player_list.append(
+                InputBox(info[0], info[1], info[2], info[3], i, 'player'+str(i), self.font))
+
+    def display(self, main):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                 pygame.quit()
-                 exit()
+                pygame.quit()
+                exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.start_button.handle_event(event)
             for computer in self.player_list:
                 computer.handle_event(event)
             if event.type == pygame.KEYDOWN:
-                    # if event.key == pygame.K_RETURN:
-                    #     self.active = not self.active
-                    # else:
-                    #     self.active=False
-                        # Enter y키를k 누르면 Input Box의 텍스트를 출력합니다.
-                            # self.namebox[1]= self.namebox[1]
-                    # if event.ke == pygame.K_LEFT |pygame.K_RIGHT :
-                    #     self.cliced= not self.clicked
-                    if event.key == pygame.K_UP:
-                        self.index-=1    
-                    if event.key ==  pygame.K_DOWN:
-                        self.index+=1
-            
-                    if(self.index<-1):
-                        self.index=5
-                    if(self.index>5):
-                        self.index=-1  
-                    if(self.index>-1):
-                        self.player_list[self.index].handle_key_event(event,self.index)
-                        print( self.player_list[self.index])
- 
-                    if event.key == pygame.K_RETURN:
-                        if(self.index==-1):
-                            self.start_button.handle_key_event(event)
-                           
-                # if event.key == self.main.user_data.key_left | self.main.user_data.key_right:
-                    # if(self.index>0):
-                        # self.player_list[self.index].handle_key_event(event,self.index)
-                  
+                if event.key == pygame.K_UP:
+                    self.index -= 1
+                if event.key == pygame.K_DOWN:
+                    self.index += 1
 
-                    
-                    # if event.type ==  pygame.K_RETURN:
-                    #     if(self.index==-1):
-                    #         self.start_button.action()
-                    #         print('jello')
-                    # if event.key == pygame.K_BACKSPACE:
-                    #     # 백스페이스 키를 누르면 Input Box의 텍스트에서 마지막 문자를 삭제합니다.
-                    #     self.namebox[1] = self.namebox[1][:-1]
-                    # else:
-                    #     # 키 입력이 있으면 Input Box의 텍스트에 추가합니다.
-                    #     self.namebox[1] += event.unicode
-                    print(self.index)
-                                   
-        # print(self.index)
+                if (self.index < -1):
+                    self.index = 5
+                if (self.index > 5):
+                    self.index = -1
+                if (self.index > -1):
+                    self.player_list[self.index].handle_key_event(
+                        event, self.index)
+
+                if event.key == pygame.K_RETURN:
+                    if (self.index == -1):
+                        self.start_button.handle_key_event(event)
+
         main.screen.blit(self.lobby_img, (0, 0))
         for computer in self.player_list:
             computer.draw(main.screen)
-        # self.screen.blit(self.button_select_img, self.button_select_rect)
         self.start_button.draw()
-            
-    def make_screen(self,width,height,ratios):
+
+    def make_screen(self, width, height, ratios):
         for ratio in ratios:
-            self.player_box_info.append((width*ratio[0], height*ratio[1],self.screen_width/6,self.screen_heigth/12))
+            self.player_box_info.append(
+                (width*ratio[0], height*ratio[1], self.screen_width/6, self.screen_heigth/12))
+
+    def check_user(self):
+        for user in self.player_list:
+            if user.clicked == True:
+                self.count += 1
+        if self.count > 1:
+            self.play_game()
+        else:
+            self.count = 0
+
     def play_game(self):
-       self.main.add_info(self.player_list)
-       self.main.scene_change(self.main.get_scene_index("game start"))
-       
+        self.main.add_info(self.player_list)
+        self.main.scene_change(self.main.get_scene_index("game start"))
+
     def change_screen_size(self):
-        pass # title의 size 변경 방식과의 호환을 위해 임시로 만듦
+        pass  # title의 size 변경 방식과의 호환을 위해 임시로 만듦
